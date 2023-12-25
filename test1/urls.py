343a62c3a4248aecef765e0b27fa17b789fac54a
Miguel Augusto da Silva <040709augustomiguel@gmail.com>
2023-12-25 19:07:13 -0300

    add staitcfiles

--- test1/urls.py
+++ test1/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from website import urls
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
