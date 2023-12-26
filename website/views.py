1e162548b1834552e13870ca7f230ab5eaaa78f3
Miguel Augusto da Silva <040709augustomiguel@gmail.com>
2023-12-26 02:07:22 -0300

    voltando

--- website/views.py
+++ website/views.py
@@ -3,12 +3,4 @@ from . import models
 
 def index(request):
     pessoa = models.Pessoas.objects.all()
-    imagens_carosels = models.Imagem_carosel.objects.all()
-    for imagem in imagens_carosels:
-        if imagem.lugar == "pri":
-            pri = imagem.img.url
-        if imagem.lugar == "seg":
-            seg = imagem.img.url
-        if imagem.lugar == "ter":
-            ter = imagem.img.url
-    return render(request, 'index.html', {'pessoas': pessoa, "pri": pri, "seg": seg, "ter": ter})
+    return render(request, 'index.html', {'pessoas': pessoa})
