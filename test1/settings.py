d128a6a5612ee9651708ad9fb97afa99975a2c9e
Miguel Augusto da Silva <040709augustomiguel@gmail.com>
2023-12-26 02:01:38 -0300

    test image ksksk

--- test1/settings.py
+++ test1/settings.py
@@ -126,6 +126,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ] 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Pessoas
+from .models import Pessoas, Imagem_carosel
 
 admin.site.register(Pessoas)
+
+@admin.register(Imagem_carosel)
+class EventosAdmin(admin.ModelAdmin):
+    actions = [remover_imagem,]
