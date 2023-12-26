7a3ad2d870d9c16af452cc4e17ea57ac8cf4e03f
Miguel Augusto da Silva <040709augustomiguel@gmail.com>
2023-12-26 02:03:46 -0300

    test image add action

--- website/admin.py
+++ website/admin.py
@@ -3,6 +3,12 @@ from .models import Pessoas, Imagem_carosel
 
 admin.site.register(Pessoas)
 
+@admin.action(description="remover imagem junto")
+def remover_imagem(modeladmin, request, queryset):
+    for evento in queryset:
+        print(f"{settings.MEDIA_ROOT}/{evento.img}")
+        os.remove(f"{settings.MEDIA_ROOT}/{evento.img}")
+
 @admin.register(Imagem_carosel)
 class EventosAdmin(admin.ModelAdmin):
     actions = [remover_imagem,]
